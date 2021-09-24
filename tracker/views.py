5417abb1f53c3f69aa8216fd024e6fe697a7533b
plas318 <plas3180g@gmail.com>
2021-09-24 20:13:08 +0900

    all set

--- tracker/views.py
+++ tracker/views.py
@@ -3,7 +3,10 @@ from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
-
+@views.route('/')
+def home():
+    return ''
 @views.route('/tracker') # Flask route to tracker page
 def tracker(): # methods=['POST', 'GET'] 
-    return render_template('index.html') # return rendered website 
+    return render_template('index.html') 
+# return rendered website 
